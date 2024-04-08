7a692aa2dfba7bed347560fcafb209f10e55ed32
MrHighlandCN <trancaonguyen0901@gmail.com>
2024-04-08 21:49:53 +0700

    First commit, view document function

--- source/angular/src/app/main/documents/documents.module.ts
+++ source/angular/src/app/main/documents/documents.module.ts
@@ -3,9 +3,10 @@ import {AppSharedModule} from '@app/shared/app-shared.module';
 import {DocumentsRoutingModule} from './documents-routing.module';
 import {DocumentsComponent} from './documents.component';
 import {SubheaderModule} from '@app/shared/common/sub-header/subheader.module'
+import { ViewDocumentModalComponent } from './view-document-modal.component';
 
 @NgModule({
-    declarations: [DocumentsComponent],
+    declarations: [DocumentsComponent, ViewDocumentModalComponent],
     imports: [AppSharedModule, DocumentsRoutingModule, SubheaderModule]
 })
 export class DocumentsModule {}
